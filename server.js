0871e8c68a590018029e241f57424c22f2a65c4d
denieall j <denieall.joe@gmail.com>
2018-08-19 12:57:29 +0800

    Intial commit

---
+++ server.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+//maintenance
+//live
+let mode = "live";
+
+let app = express();
+
+// Partial HTML page
+hbs.registerPartials(__dirname + "/views/partials");
+
+// helper
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
+hbs.registerHelper('upCASE', (str) => str.toUpperCase());
+
+// By defaults render from folder named: views
+app.set('view engine', 'hbs');
+
+// Middleware
+// Where user authentication, logs and db connection is performed
+// App wont run until this middleware is completed ie next function is called
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    let log = req.method + " --- " + req.url + ", " + now;
+
+    fs.appendFile('./server.log', log + '\n\n', (err) => {
+        if(err) {
+            console.log("Failed");
+        }
+    });
+
+    next();
+});
+
+app.use((req, res, next) => {
+    if (mode === "live") {
+
+        next();
+
+    } else {
+        res.render("maintenance.hbs");
+    }
+});
+
+
+app.use(express.static(__dirname + "/public"));
+
+
+// Routes
+app.get('/', (req, res) => {
+
+    res.render('home.hbs', {
+        pageTitle: "Home Page",
+        welcomeMsg: "Hi, there!"
+    });
+
+});
+
+app.get('/about', (req, res) => {
+
+    res.render('about.hbs', {
+        pageTitle: "About Page",
+    });
+
+});
+
+app.get('/bad', (req, res) => {
+
+    res.send({request: 404});
+
+});
+
+app.listen(3000, () => {
+    console.log("Listening on port: 3000");
+    console.log("URL: http://localhost:3000");
+});
