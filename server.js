e6bf1490e83e4b7bc8e74eb72518f5d8ef064d72
denieall j <denieall.joe@gmail.com>
2018-08-20 01:06:26 +0800

    Add projects web page

--- server.js
+++ server.js
@@ -69,6 +69,15 @@ app.get('/about', (req, res) => {
 
 });
 
+app.get('/projects', (req, res) => {
+
+    res.render('projects.hbs', {
+        pageTitle: "Project Page",
+    });
+
+});
+
+
 app.get('/bad', (req, res) => {
 
     res.send({request: 404});
